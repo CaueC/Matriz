8017288f91772b33494f6217247cf4c5dcaa827d
CaueC <caue.coube@fatec.sp.gov.br>
2022-03-21 18:02:11 +0000

    Salvando

---
+++ Tabuada.java
@@ -0,0 +1,15 @@
+public class Tabuada {
+    public static void main(String[]args) {
+
+        int idade[]=new int [11];
+
+        for (int i = 0; i < idades.length; i++) {
+            idades[i]=i*10;
+
+        }
+        for (int i=0; i < idades.length; i++) {
+            System.out.println(idades)
+            ;      
+        }
+    }
+}
